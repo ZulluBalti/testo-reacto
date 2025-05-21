393c5c47cbebcd50d4c82d90e91299713d1cdda8
ZulluBalti <zullubalti@gmail.com>
2025-05-21 20:10:28 +0500

    update title

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Zullu D. Great + React</h1>
+      <h1>Zullu D. Great</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
